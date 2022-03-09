2c5a0151f07c874747f6e4a53ed791dcbea0ea9f
COMA <aaaaamohammadaaaaa@gmail.com>
2022-03-09 04:04:16 +0330

    Update Article4-Main.jsx

--- src/components/MAIN-component/Article4-Main/Article4-Main.jsx
+++ src/components/MAIN-component/Article4-Main/Article4-Main.jsx
@@ -30,7 +30,7 @@ const Article4Main = () => {
         <h1 className="h1-Article4-Main">Get early access today</h1>
         <p className="p-Article4-Main">It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.</p>  
           <form action="" method="post" className="form-Article4-Main">
-            <input type="email" name="" id="" onChange={(e) => Setvalue(e.target.value)} value={value} className="input-Article4-Main" placeholder="email@example.com" />
+            <input type="email" required name="" id="" onChange={(e) => Setvalue(e.target.value)} value={value} className="input-Article4-Main" placeholder="email@example.com" />
             <p className="p-valid-email" id="Message">please enter a valid email address</p>
             <button type="submit" className="BTN-Article4-Main" id="BTNsubmit">Get Started For Free</button>
           </form>
